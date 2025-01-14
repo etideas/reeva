88ec0ac9d04446903ddc0269ac430482f3587b6f
soumi-learner <75557834+soumi-learner@users.noreply.github.com>
2025-01-14 21:04:57 +0530

    new text added

--- Frontend/src/components/Motion.jsx
+++ Frontend/src/components/Motion.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import img1 from "../assets/img/img1.jpg";
+import img1 from "../assets/img/img17.jpg";
 import homeVideo from "../assets/HomeVid.mov";
 
 const motionSettings = {
@@ -14,33 +14,6 @@ const motionSettings = {
 };
 
 const Motion = () => {
-  const cardData = [
-    {
-      imgSrc: "/images/PHOTO 2-1.jpg",
-      altText: "Special Private Tour",
-      title: "The Reeva Life",
-      description: `In 2022, we—Gaurav, Vaidehi, and Kaeya—left the traditional
-              lifestyle behind and embraced a life at sea on a 42-foot sailboat.
-              Believing in adventurous living, we sold everything we owned to
-              travel the world, experiencing the beauty and challenges of life
-              on the ocean`,
-    },
-    {
-      imgSrc: "/images/PHOTO 3.PNG",
-      altText: "Customized Group Hiking",
-      title: "Customized Group Hiking",
-      description:
-        "While we go along, we intend to contribute by spreading awareness. Just sharing ideas about little changes in our lifestyle that can go a long way in making this world a better place.",
-    },
-    {
-      imgSrc: img1,
-      altText: "Reeva Family",
-      title: "Reeva Family",
-      description:
-        "This life at sea offers us freedom, simplicity, and a daily dose of adventure as we navigate challenges, while discovering offbeat destinations.",
-    },
-  ];
-
   return (
     <div id="home">
       <section className="relative h-screen bg-fixed m-0 p-0 overflow-hidden">
@@ -65,39 +38,86 @@ const Motion = () => {
       </section>
 
       <div className="bg-[url('./assets/bg5.gif')] bg-no-repeat bg-cover px-10">
-        <div className="flex flex-col items-center pt-11">
-          {cardData.map(({ imgSrc, altText, title, description }, index) => (
-            <motion.section
-              key={index}
-              className={`flex flex-col ${
-                index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
-              } items-center justify-between w-full gap-10 py-10`}
+
+
+
+      
+        <div className="flex flex-col items-center pt-[7rem]">
+
+          {/* First Card */}
+          <motion.section
+            className="flex flex-col md:flex-row items-center justify-between w-full gap-8 py-8"
+            {...motionSettings}
+          >
+            <motion.img
+              src="/images/PHOTO 2-1.jpg"
+              alt="Special Private Tour"
+              className="w-[800px] md:w-2/2 h-[450px] object-cover rounded-lg shadow-lg border-t-[10px] border-b-[28px] border-r-[10px] border-white"
+              {...motionSettings}
+            />
+            <motion.div
+              className="text-white md:w-1/2 px-6 text-center"
+              {...motionSettings}
             >
-              <motion.img
-                src={imgSrc}
-                alt={altText}
-                className={`w-[783px] md:w-2/2 h-[450px] object-cover rounded-lg shadow-lg ${
-                  index === 1
-                    ? "border-t-[20px] border-b-[40px] border-l-[20px] border-blue-200" // Custom border for the second image
-                    : "border-t-[13px] border-b-[30px] border-r-[13px] border-blue-200" // Default border for others
-                }`}
-                {...motionSettings}
-              />
-              <motion.div
-                className="text-white md:w-1/2 px-6 text-center"
-                {...motionSettings}
-              >
-                {title && (
-                  <h2 className="text-sm md:text-2xl lg:text-3xl font-bold mb-4">
-                    {title}
-                  </h2>
-                )}
-                <p className="text-sm md:text-base lg:text-lg leading-relaxed">
-                  {description}
-                </p>
-              </motion.div>
-            </motion.section>
-          ))}
+              <h2 className="text-sm md:text-2xl lg:text-3xl font-bold mb-4">
+                The Reeva Life
+              </h2>
+              <p className="text-sm md:text-base lg:text-lg leading-relaxed">
+              We are Kaeya, Vaidehi and Gaurav, a family of three, who have decided to move away from the normal and adopt an alternate lifestyle. A life at sea. We always believed that all that we buy/acquire comes at a cost far greater than the MRP. A price that is being paid by this beautiful planet of ours. We thus quit our jobs sold everything that we owned and moved to live on a 42 foot sail boat, to explore cultures, cuisines and that too on a shoestring budget. This is the story of our adventures as we cross oceans, and laugh our way through the thick and thin of a liveaboard lifestyle.
+              </p>
+            </motion.div>
+          </motion.section>
+
+          {/* Second Card */}
+          <motion.section
+            className="flex flex-col md:flex-row-reverse items-center justify-between w-full gap-8 py-8 pt-14" 
+            {...motionSettings}
+          >
+            <motion.img
+              src="/images/PHOTO 3.PNG"
+              alt="Customized Group Hiking"
+              className="w-[800px] md:w-2/2 h-[450px] rounded-lg shadow-lg border-t-[10px] border-b-[28px] border-l-[10px] border-white"
+              {...motionSettings}
+            />
+            <motion.div
+              className="text-white md:w-1/2 px-6 text-center"
+              {...motionSettings}
+            >
+              <h2 className="text-sm md:text-2xl lg:text-3xl font-bold mb-4">
+                Customized Group Hiking
+              </h2>
+              <p className="text-sm md:text-base lg:text-lg leading-relaxed">
+              This lifestyle gives us the freedom to travel around the world while we use renewable energy for all our needs. As we go from island to island, country to country we like to interact with local communities; learning, sharing, improving. All this with an aim to gradually be able to further reduce our carbon footprint and educate those we meet about environmental conservation.
+              </p>
+            </motion.div>
+          </motion.section>
+
+          {/* Third Card */}
+          <motion.section
+            className="flex flex-col md:flex-row items-center justify-between w-full gap-8 py-8"
+            {...motionSettings}
+          >
+            <motion.img
+              src={img1}
+              alt="Reeva Family"
+              className="w-[800px] md:w-2/2 h-[450px] rounded-lg shadow-lg border-t-[10px] border-b-[28px] border-r-[10px] border-white"
+              {...motionSettings}
+            />
+            <motion.div
+              className="text-white md:w-1/2 px-6 text-center"
+              {...motionSettings}
+            >
+              <h2 className="text-sm md:text-2xl lg:text-3xl font-bold mb-4">
+              About our boat
+              </h2>
+              <p className="text-sm md:text-base lg:text-lg leading-relaxed">
+              Reeva is a 1988 built, Tayana V 42. It’s an aft cockpit canoe stern design and was custom built for living onboard while sailing in all-weather conditions. She was Hull No. 156 of the 168 boat that were built with this design and most are still in use. They are so versatile and capable that you will be surprised to find them in some of the remotest islands around the world. She might not be fast, but, she is sure.
+              </p>
+            </motion.div>
+          </motion.section>
+
+         
+
         </div>
       </div>
     </div>
